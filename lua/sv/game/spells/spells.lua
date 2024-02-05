54bc161824d8c778fc19f95593cba32a8f530cc1
RBLOKO <maxantialeks@gmail.com>
2024-02-06 00:13:01 +0300

    Add into spells.lua and set reload time

--- lua/sv/game/spells/spells.lua
+++ lua/sv/game/spells/spells.lua
@@ -2,5 +2,12 @@ AddSpell({
 	["Cmd"] = "slowdown",
 	["Projectile"] = "basic_slowdown",
 	["ManaCost"] = 10,
-	["ReloadTime"] = 1  -- Seconds
+	["ReloadTime"] = 5  -- Seconds
+})
+
+AddSpell({
+	["Cmd"] = "wall",
+	["Projectile"] = "basic_wall",
+	["ManaCost"] = 15,
+	["ReloadTime"] = 10  -- Seconds
 })
